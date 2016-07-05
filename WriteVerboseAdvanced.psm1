68d778ae826ff690fea88959aafbc6f36ceabf35
ChrisMagnuson <hourglasssand@gmail.com>
2016-07-05 13:55:55 -0400

    Initial commit

---
+++ WriteVerboseAdvanced.psm1
@@ -0,0 +1,11 @@
+﻿function Write-VerboseAdvanced {
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory, ValueFromPipeline)]$Input,
+        [Switch]$PassThrough
+    )
+    if($VerbosePreference -ne "SilentlyContinue") {
+        $Input | Format-List * | Out-String -Stream | Write-Verbose
+    }
+    if ($PassThrough) {$Input}
+}
